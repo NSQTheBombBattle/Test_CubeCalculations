b455b56da5fa4cd0d5e21bcb24bfb88d1a86ca13
NSQTheBombBattle <elina@thebombbattle.com>
2025-03-24 11:40:47 +0800

    feat: basic drag and drop

---
+++ Assets/NumberCube.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberCube : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Vector3 offset;
+    private Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void OnMouseDown()
+    {
+        isDragging = true;
+        offset = transform.position - GetMouseWorldPos();
+    }
+
+    void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    void Update()
+    {
+        if (isDragging)
+        {
+            transform.position = GetMouseWorldPos() + offset;
+        }
+    }
+
+    Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = -mainCamera.transform.position.z; // Ensure correct depth
+        return mainCamera.ScreenToWorldPoint(mousePoint);
+    }
+}
